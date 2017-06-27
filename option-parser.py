de48c1ed2dae567ac8ed3eb13840a67163479816
Hernan Gallo <hgallo@devartis.com>
2017-06-27 15:01:47 -0300

    Dos scripts basicos de python

---
+++ option-parser.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+import sys
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-n", "--name",dest="name",help="Your Name")
+
+(options,args) = parser.parse_args()
+
+print "Hi " + options.name
